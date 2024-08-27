9de2afa30775ee4eae54af6b13fafaf8d1ad9096
DinCyDab <19103291@usc.edu.ph>
2024-08-27 21:34:28 +0800

    updated BasicMathOperations

--- src/ArithmeticOP/BasicMathOperations.java
+++ src/ArithmeticOP/BasicMathOperations.java
@@ -13,12 +13,48 @@ public class BasicMathOperations {
         return a + b;
     }
     
+    public int sub(int a, int b){
+        return a - b;
+    }
+    
+    public int mul(int a, int b){
+        return a * b;
+    }
+    
+    public int div(int a, int b){
+        return a / b;
+    }
+    
     public double add(double a, double b){
         return a + b;
     }
     
+    public double sub(double a, double b){
+        return a - b;
+    }
+    
+    public double mul(double a, double b){
+        return a * b;
+    }
+    
+    public double div(double a, double b){
+        return a / b;
+    }
+    
     public float add(float a, float b){
         return a + b;
+    }
+    
+    public float sub(float a, float b){
+        return a - b;
+    }
+    
+    public float mul(float a, float b){
+        return a * b;
+    }
+    
+    public float div(float a, float b){
+        return a / b;
     }
     
     public long add(long a, long b){
